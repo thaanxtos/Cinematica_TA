f010c9e0cd3c4306a4c8235b4897447d10cd627f
thaanxtos <thaanxtos.mc@gmail.com>
2026-03-12 19:30:28 +0100

    arreglos

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -2,21 +2,30 @@ using UnityEngine;
 
 public class Particulas : MonoBehaviour
 {
-    private float v0; //velocidad inicial
-    private float angulo0; //angulo incial
-    private float tM; //tiempo de vida maximo
-    private float tA; //tiempo que lleva la particula activa
-    private float g; //gravedad
+    ParticulasController controller;
+    public float v0X, v0Y; //velocidad inicial en x e y
+    public float angulo0; //angulo incial
+    public float tM; //tiempo de vida maximo
+    public float tA; //tiempo que lleva la particula activa
+    public float tT; //tiempo total
+    public float g; //gravedad
 
-    void Start() 
-    {
-
-        g = 9.8f;
+    public void SetValues(ParticulasController controller, float v0P, float angulo0P, float tLP0, float gP)
+    { 
+        this.controller = controller;
+        this.angulo0 = angulo0P;
+        this.tM = tLP0;
+        this.tA = tLP0;
+        this.g = gP;
+        this.v0X = v0P * Mathf.Cos(angulo0P * Mathf.Deg2Rad);
+        this.v0Y = v0P * Mathf.Sin(angulo0P * Mathf.Deg2Rad);
+     
+        tT = 0;
     }
-
     
     void Update() 
     {
-        
+        tT += Time.deltaTime;
+        controller.UpdateParticlePositions(this, tT);
     }
 }
--- Assets/Scripts/ParticlesController.cs
+++ Assets/Scripts/ParticlesController.cs
@@ -1,5 +1,6 @@
-using NUnit.Framework;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class ParticulasController : MonoBehaviour
@@ -7,34 +8,43 @@ public class ParticulasController : MonoBehaviour
     public int nP; //numero de particulas
     public float v0P; //velocidad inicial de las particulas
     public float angulo0P; //angulo incial de las particulas
-    public float tLP; //tiempo de vida (life) de las particulas
+    public float tLP0; //tiempo de vida (life) inicial de las particulas
     public float gP; //gravedad de las particulas
-    GameObject Particulas;
+    public GameObject Particulas; //prefab de la particula
+    Particulas ultimaPart; //referencia a la ultima particula creada
 
-    void Start() //añadir array con particulas
+    public void onActivate(InputAction.CallbackContext context)
     {
-        Particulas p = new Particulas();
-        CreateParticleExplosion(new List<GameObject>());
-    }
-    
-    void Update() //actualizar el array 
-    {
-        UpdateParticlePositions(new Particulas(), Time.deltaTime);
-    }
-
-    void CreateParticleExplosion(List<GameObject> particleList)
-    {
-        particleList = new List<GameObject>();
-        for (int i = 0; i < nP; i++) //añadir particulas a la lista según nP
+        if (context.performed)
         {
-            GameObject particle = new GameObject("Particula" + i);
-            particle.AddComponent<Particulas>();
-            particleList.Add(particle);
+            CreateParticleExplosion();
         }
     }
 
-    void UpdateParticlePositions(Particulas p, float time)
+    void CreateParticleExplosion()
     {
-        time = time * Time.deltaTime;
+        for (int i = 0; i < nP; i++)
+        {
+            ultimaPart = Instantiate(Particulas, transform).GetComponent<Particulas>();
+            //el random range inicial es la variacion del angulo
+            float angulo = angulo0P + Random.Range(5f, 50f) * Mathf.Sqrt(-2f * Mathf.Log(Random.value)) * Mathf.Cos(2f * Mathf.PI * Random.value); 
+            float velocidad = Mathf.Max(v0P + Random.Range(-5f, 5f) * Mathf.Sqrt(-2f * Mathf.Log(Random.value)) * Mathf.Cos(2f * Mathf.PI * Random.value), 0f);
+            float tiempoVida = tLP0 + Random.Range(-1f, 1f) * Mathf.Sqrt(-2f * Mathf.Log(Random.value)) * Mathf.Cos(2f * Mathf.PI * Random.value);
+            ultimaPart.SetValues(this, velocidad, angulo, tiempoVida, gP);
+        }
+    }
+
+    public void UpdateParticlePositions(Particulas p, float time)
+    {
+        if(p.tT < time)
+        {
+            p.gameObject.SetActive(false);
+            return;
+        }
+
+        Vector3 newPosition = p.transform.position;
+        newPosition.x = p.v0X * time;
+        newPosition.y = (p.v0Y * time) - (p.g * time * time/2);
+        p.transform.position = newPosition;
     }
 }
